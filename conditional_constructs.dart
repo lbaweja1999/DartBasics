3ac35de6cff65b7355f06cb2b4f31516270b20e4
Lakshay Baweja <lakshay17csu093@ncuindia.edu>
2020-05-25 12:28:19 +0530

    if else in dart

---
+++ conditional_constructs.dart
@@ -0,0 +1,16 @@
+main(){
+  if(3>4){
+    print("Process statement when condition is TRUE");
+  }
+  else{
+    print("Process statement when condition is FALSE");
+  }
+  int ch = 3;
+  switch (ch){
+    case 1: print("One");
+            break;
+    case 2: print("two");
+            break;
+    case 3: print("three");
+  }
+}
