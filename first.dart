5d78cb6f8cdd1467d695747efd881f8c365388c7
Lakshay Baweja <lakshay17csu093@ncuindia.edu>
2020-05-25 12:23:19 +0530

    hello world in dart

---
+++ first.dart
@@ -0,0 +1,21 @@
+main(){
+  print("Hello World");
+}
+
+
+/*   
+Topics
+=======
+First Program
+Data Types
+Number - int double
+String
+Boolean true/ false
+sets
+List
+Map
+
+Conditional constructs   if-else   switch case
+Looping constructs   for  while  do-while   for each
+
+*/
