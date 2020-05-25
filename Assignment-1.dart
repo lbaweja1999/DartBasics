0ea12e45c45f656a4a6c81efa202520298f68b51
Lakshay Baweja <lakshay17csu093@ncuindia.edu>
2020-05-25 17:11:41 +0530

    Assignment-1 dart

---
+++ Assignment-1.dart
@@ -0,0 +1,10 @@
+main() {
+  print(
+      "Q1.Create variables of type String, Integer, double and Boolean.Print the values of these variableby a single print statement.\n");
+  String name = "Lakshay";
+  int age = 20;
+  double age_double = 20.0;
+  bool yes = true;
+  print(
+      "My name is ${name}.I am ${age} years old.Age in double is ${age_double}.And yes its all ${yes}.");
+}
